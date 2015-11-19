cf90fde97466a2913cc851fac2ef6c12fbf1e920
Angamanga <ki222bj@student.lnu.se>
2015-11-19 11:52:54 +0100

    simple form with two way binding

--- index.js
+++ index.js
@@ -2,18 +2,88 @@
 var React = require("react"),
     ReactDOM = require("react-dom");
 
-var HelloWorld = React.createClass({
-    render: function() {
+var NewAdvertForm = React.createClass({
+    handleChange: function() {
+            this.props.onUserInput(
+            this.refs.titleText.value,
+            this.refs.descriptionText.value,
+            this.refs.nameText.value
+        );
+    },
+        render: function() {
         return (
         <div>
-        <h1>Hello, world!</h1>
-        <h3>Its working!! What an awesome feeling!!!</h3>
+        <form>
+            <legend for="title">Titel</legend>
+            <input
+                name="title"
+                type="text"
+                ref="titleText"
+                onChange={this.handleChange}
+
+            />
+            <legend for="description">Beskrivning</legend>
+            <input
+                name="description"
+                type="text"
+                ref="descriptionText"
+                onChange={this.handleChange}
+            />
+            <legend for="name">Ditt namn</legend>
+            <input
+                name="name"
+                type="text"
+                ref="nameText"
+                onChange={this.handleChange}
+            />
+        </form>
         </div>
         );
     }
 });
 
+var PreviewAdvert = React.createClass({
+    render:function(){
+        return (<div>
+            <h1>{this.props.titleText}</h1>
+            <p>{this.props.descriptionText}</p>
+            <p>{this.props.nameText}</p>
+            </div>
+        )
+    }
+})
+var RegisterAdvertPage = React.createClass({
+    getInitialState: function() {
+        return {
+            titleText: 'Här kommer beskrivningen av annonsen att synas',
+            descriptionText:'Här kommer beskrivningen av annonsen att synas',
+            nameText:'Här kommer ditt namn att synas'
+            };
+    },
+    handleUserInput: function(titleText, descriptionText,nameText) {
+        this.setState({
+            titleText: titleText,
+            descriptionText: descriptionText,
+            nameText:nameText
+        });
+    },
+    render:function(){
+        return (
+            <div>
+        <NewAdvertForm
+            onUserInput={this.handleUserInput}
+        />
+        <PreviewAdvert
+            titleText={this.state.titleText}
+            descriptionText={this.state.descriptionText}
+            nameText={this.state.nameText}
+        />
+        </div>
+                )
+    }
+})
+
 ReactDOM.render(
-<HelloWorld />,
+    <RegisterAdvertPage />,
     document.getElementById('hello')
 );
