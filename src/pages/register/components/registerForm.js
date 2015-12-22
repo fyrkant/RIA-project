9746e0b2c2baffff328958d0fbc9f6d959d39c99
Angamanga <ki222bj@student.lnu.se>
2015-12-22 22:32:23 +0100

    error again

--- src/pages/register/components/registerForm.js
+++ src/pages/register/components/registerForm.js
@@ -2,14 +2,14 @@ var React = require("react"),
     ReactDOM = require("react-dom");
 
 var RegisterUserForm = React.createClass({
-//    getInitialState:function(){
-//        return
-//        {
-//            emailmessage: '',
-//            emailColor: ''
-//        };
-//        
-//    },
+    getInitialState:function(){
+        return
+        {
+            emailmessage: '',
+            emailColor: ''
+        };
+        
+    },
 //    handleChange:function(){
 //        this.props.onUserInput(
 //        this.refs.emailInput,
@@ -54,7 +54,7 @@ var RegisterUserForm = React.createClass({
                             name="email"
                             type="email"
                             ref="emailInput"
-//                            className={this.state.emailColor}
+                            className={this.state.emailColor}
 //                            onBlur={this.validateEmail}
 //                            onChange={this.handleChange}
             
@@ -69,7 +69,7 @@ var RegisterUserForm = React.createClass({
                         />
                         
                    
-
+<p>{this.state.emailmessage}</p>
                         <legend htmlFor="password">Password</legend>
                         <input
                             name="password"
