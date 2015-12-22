77d1c8acc4019c9063b854203ba1747d34c32484
Angamanga <ki222bj@student.lnu.se>
2015-12-22 22:15:54 +0100

    delete old files

--- src/views/stuff.js
+++
@@ -1,6 +0,0 @@
-
-var STUFF = {
-{title:'Strumpor',description:'Fina strumpor bortskänkes',name:'Anna'},
-{title:'Elvisp',description:'Trasig elvisp bortskankes', name:'Edit'},
-{title:'Cykel',description:'Helt ny cykel bortskankes', name:'Per'}
-};
